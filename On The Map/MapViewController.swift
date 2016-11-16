ec85b0f1465d547a9ad3a82e4d68ebfad36b9a59
LIJO RAJU <lijoraju@LIJOs-MacBook-Air.local>
2016-11-16 12:50:10 +0530

    Shows studentLocation as pins in map

--- On The Map/MapViewController.swift
+++ On The Map/MapViewController.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        mapView.delegate = self
+        reloadMapView()
+    }
+    
+    func reloadMapView() {
+        
+        // Clear previous annotations
+       // let allAnnotations = self.mapView.annotations
+      //  self.mapView.removeAnnotation(allAnnotations as! MKAnnotation)
+        for result in StudentsData.sharedInstance().mapPins {
+            
+            // Create annotation
+            let annotation = MKPointAnnotation()
+            
+            // Set the coordinate and add it to annotation
+            let location = CLLocationCoordinate2D(latitude: result.Latitude, longitude: result.Longitude)
+            annotation.coordinate = location
+            
+            // Add student name and mediaURL for annotation
+            annotation.title = result.Name
+            annotation.subtitle = result.mediaURL
+            
+            // Add annotation to map
+            mapView.addAnnotation(annotation)
+        }
+        
+    }
+    
+}
